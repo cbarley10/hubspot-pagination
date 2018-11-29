7e7e6d9a00087bf41768b7672707e44bc40ed39d
Connor Barley <cbarley@hubspot.com>
2018-11-29 17:24:53 -0500

    refactored app.js

--- app.js
+++ app.js
@@ -5,37 +5,49 @@ const hapikey = process.env.API_KEY;
 
 const property = "email";
 const object = "contacts";
-const contactsUrl = `https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=${hapikey}&count=100&property=${property}`;
+const mainUrl = `https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=${hapikey}&count=100&property=${property}`;
 const arr = [];
 
+const loopThroughObjects = (data) => {
+  data[object].forEach((item) => {
+    if(item.properties[property] && item.properties[property].value){
+      arr.push(item.properties[property].value)
+    }
+  });
+}
+
+const appendData = (file, logs, array) => {
+  fs.appendFile(file, "\n" + logs + "\n\n" + array, (err) => {
+    if(err) {
+      console.log("Unable to append to server log");
+    }
+  });
+}
+
 const fetchData = (url) => {
-  return axios.get(url).then(response => {
+  return axios.get(url)
+  .then(response => {
     const data = response.data;
-    data[object].forEach((item) => {
-      if(item.properties[property]){
-        if(item.properties[property].value){
-          arr.push(item.properties[property].value);
-        }
-      }
-    });
-    const newUrl = `${contactsUrl}&vidOffset=${data["vid-offset"]}`;
+    loopThroughObjects(data);
+    const newUrl = `${mainUrl}&vidOffset=${data["vid-offset"]}`;
     if (data["has-more"] === true) return fetchData(newUrl);
     else return data;
+  })
+  .catch((err) => {
+    console.log(error);
   })
 };
 
 const main = async () => {
-  fetchData(contactsUrl).then((result) => {
+  fetchData(mainUrl)
+  .then(() => {
     let now = new Date().toString();
-    let log = `Time Now: ${now}. Contacts with value returned: ${arr.length}`
+    let log = `Time Now: ${now}. ${object.charAt(0).toUpperCase() + object.slice(1)} with value returned: ${arr.length}`
     console.log(log);
     console.log(arr);
-    fs.appendFile("server.log", log + "\n\n" + arr, (err) => {
-      if(err) {
-        console.log("Unable to append to server log");
-      }
-    });
-  }).catch((error) => {
+    appendData("server.log", log, arr);
+  })
+  .catch((error) => {
     console.log(error);
   });
 }
